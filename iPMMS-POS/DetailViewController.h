4b61f91ff8505c11f97d02fd9ef054d3b3725796
Macintosh User <macuser@Mac-Mini-32.local>
2012-08-12 17:27:19 +0530

    Initial Commit

---
+++ iPMMS-POS/DetailViewController.h
@@ -0,0 +1,17 @@
+//
+//  DetailViewController.h
+//  iPMMS-POS
+//
+//  Created by Macintosh User on 12/8/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DetailViewController : UIViewController <UISplitViewControllerDelegate>
+
+@property (strong, nonatomic) id detailItem;
+
+@property (strong, nonatomic) IBOutlet UILabel *detailDescriptionLabel;
+
+@end
---
+++ iPMMS-POS/MasterViewController.h
@@ -0,0 +1,17 @@
+//
+//  MasterViewController.h
+//  iPMMS-POS
+//
+//  Created by Macintosh User on 12/8/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class DetailViewController;
+
+@interface MasterViewController : UITableViewController
+
+@property (strong, nonatomic) DetailViewController *detailViewController;
+
+@end
