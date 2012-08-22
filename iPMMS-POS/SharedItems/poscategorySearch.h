51653019a79f7c38590533884678164c4ad1caac
Macintosh User <macuser@Mac-Mini-32.local>
2012-08-22 14:52:49 +0530

    xx

---
+++ iPMMS-POS/SharedItems/poscategorySearch.h
@@ -0,0 +1,13 @@
+//
+//  poscategorySearch.h
+//  iPMMS-POS
+//
+//  Created by Macintosh User on 22/8/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface poscategorySearch : UIView
+
+@end
---
+++ iPMMS-POS/SharedItems/poscategorySearch.m
@@ -0,0 +1,31 @@
+//
+//  poscategorySearch.m
+//  iPMMS-POS
+//
+//  Created by Macintosh User on 22/8/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "poscategorySearch.h"
+
+@implementation poscategorySearch
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
+{
+    // Drawing code
+}
+*/
+
+@end
